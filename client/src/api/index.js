858f4d2a2d257ff370f0040114c0a993e7761d7f
dhimzikri <dschok182@gmail.com>
2022-09-22 12:05:36 +0700

    addAxios

---
+++ client/src/api/index.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+const baseURL = "http://localhost:4000/"
+
+export const validateUser = async (token) => {
+    try {
+        const res = await axios.get(`${baseURL}api/users/login`, {
+            headers : {
+                
+            }
+        }) 
+    } catch (error) {
+        
+    }
+}
